8a39c9589f2782aadd84d83227ee30f15957a618
Vivek Mehto <vivekmehto.webdev@gmail.com>
2025-07-10 13:59:10 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("h1", { id: "head" }, "Hello World from react"),
+  React.createElement("h2", { id: "head2" }, "Hello World h2 from react"),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
