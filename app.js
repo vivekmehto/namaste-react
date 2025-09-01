799d41fe402efc0c9a0a7bfd53212a5fdab68406
Vivek Mehto <vivekmehto.webdev@gmail.com>
2025-09-01 19:25:02 +0530

    using npm

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("h1", { id: "head" }, "Hello World from react"),
   React.createElement("h2", { id: "head2" }, "Hello World h2 from react"),
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
+ 
