20e37a2f372c13559759da0b1789f594c425116d
Vivek Mehto <vivekmehto.webdev@gmail.com>
2025-09-02 14:56:38 +0530

    Functional Component

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 
 // React.createElement => OBJECT => HTML Element(render)
 
-const heading = React.createElement("h1", {id: "heading"}, "Vivek Mehto");
+// const heading = React.createElement("h1", {id: "heading"}, "Vivek Mehto");
 
-console.log(heading);
+// console.log(heading);
 
 // JSX is not HTML but its HTML like syntax
 
-// JSX (transpiled before it reaches the JS ) -> Parcel uses  Babel and 
+// JSX (transpiled before it reaches the JS ) -> Parcel uses  Babel and
 // Babel transpiles the JSX into React.createElement and then into JS Object and then into HTML ELement
 
 // JSX -> React.createElement -> JS Object -> HTML Element
@@ -18,6 +18,24 @@ const jsxHeading = <h1 id="heading">Namaste React using JSX </h1>;
 
 console.log(jsxHeading);
 
+// Functional Comoponent
+
+const elem = <span>React ELement</span>;
+
+const Title = () => (
+  <h1 className="title">
+    {elem}
+    This is our title
+  </h1>
+);
+
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1 className="heading">Namaste React using Functional Comoponent </h1>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading)
+root.render(<HeadingComponent />);
