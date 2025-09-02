aa0f7997997dfe7faddbe17fb25fe46d878da57f
Vivek Mehto <vivekmehto.webdev@gmail.com>
2025-09-02 12:45:59 +0530

    JSX

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("h1", { id: "head" }, "Hello World from react"),
-  React.createElement("h2", { id: "head2" }, "Hello World h2 from react"),
-]);
+// React.createElement => OBJECT => HTML Element(render)
+
+const heading = React.createElement("h1", {id: "heading"}, "Vivek Mehto");
+
+console.log(heading);
+
+// JSX is not HTML but its HTML like syntax
+
+// JSX (transpiled before it reaches the JS ) -> Parcel uses  Babel and 
+// Babel transpiles the JSX into React.createElement and then into JS Object and then into HTML ELement
+
+// JSX -> React.createElement -> JS Object -> HTML Element
+
+const jsxHeading = <h1 id="heading">Namaste React using JSX </h1>;
+
+console.log(jsxHeading);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
- 
+
+root.render(jsxHeading)
